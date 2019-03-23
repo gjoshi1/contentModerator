f2414580d3a58f0a2697cefcba476fe6c5a01247
GowriKD <gowri.joshi@washpost.com>
2019-03-24 01:10:12 +0530

    Exception handling added

--- ContentModeratorAPI/src/main/java/com/target/contentModerator/controller/BlacklistContentController.java
+++ ContentModeratorAPI/src/main/java/com/target/contentModerator/controller/BlacklistContentController.java
@@ -3,16 +3,23 @@
  */
 package com.target.contentModerator.controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.target.contentModerator.model.BlacklistContent;
-import com.target.contentModerator.repository.BlacklistContentRepository;
 import com.target.contentModerator.service.BlacklistContentService;
 
 /**
@@ -31,20 +38,35 @@ public class BlacklistContentController {
 	 * 
 	 * */
 	
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+	 
 	
 	@Autowired
 	private BlacklistContentService blacklistContentService;
 	
 	
 	@RequestMapping("/create")
-	public String create(@RequestParam String word, @RequestParam String lang) {
+	public String create(@RequestParam String word, @RequestParam String lang) throws Exception {
+		
+		if(StringUtils.isEmpty(word) || StringUtils.isEmpty(lang)) {
+			throw new IllegalArgumentException("Empty Word or Language parameter");
+		}
 		
 		BlacklistContent blc = blacklistContentService.create(word, lang);
+		
+		LOGGER.info("CREATED BLACKLIST ENTRY "+blc.toString());
 		return blc.toString();
 	}
 	
 	@RequestMapping("/update")
 	public String update(@RequestBody BlacklistContent blackListContent) {
+		
+		
+		
+		if(StringUtils.isEmpty(blackListContent.getId())) {
+			throw new IllegalArgumentException("Missing ID, cannot update document");
+		}
+		
 		
 		return blacklistContentService.update(blackListContent).toString();
 	}
@@ -79,7 +101,15 @@ public class BlacklistContentController {
 	@RequestMapping("/delete")
 	public void delete(@RequestParam String id) {
 		
+		if(StringUtils.isEmpty(id.trim()) ){
+			throw new IllegalArgumentException("Missing ID, cannot delete document!");
+		}
 		blacklistContentService.delete(id);
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	void handleBadRequests(HttpServletResponse response) throws IOException {
+	    response.sendError(HttpStatus.BAD_REQUEST.value());
 	}
 
 }
